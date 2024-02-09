bdb86bfac7d89dd2d5743530e4fc5f4cc5237eb7
Israel Ibinayin <eqa5@txstate.edu>
2024-02-09 14:10:29 -0600

    Audio File works
    Fixed .gitignore to ignore geenrated mp3 files

--- intro_to_flask/create_speech_python/create_speech_route.py
+++ intro_to_flask/create_speech_python/create_speech_route.py
@@ -8,8 +8,9 @@ import sys
 import io
 sys.dont_write_bytecode = True
 from flask import render_template, request, Flask, Blueprint, Response
-from gtts import gTTS
 from .create_speech_form import SpeakmeForm
+import base64
+
 
 speech_blueprint = Blueprint('speakme', __name__)
 
@@ -26,23 +27,18 @@ def create_speech():
         # https://platform.openai.com/docs/api-reference/images
         client = OpenAI()
 
-     #   response = client.audio.speech.create(
-     #       model="tts-1",
-     #       engine="davinci",
-     #       parameters={
-     #           "voice": "alloy",  # Choose the voice
-     #       },
-     #       input = "text" = form.prompt.data,
-     #   )
+        response = openai.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input= form.prompt.data,
+        )
         
-        tts = gTTS(form.prompt.data)
-        audio_bytes = io.BytesIO()
-        tts.write_to_fp(audio_bytes)  # Write the speech to the in-memory file-like object
+        #print(response.content)
+        audio_data = response.content
 
-        audio_bytes.seek(0)  # Move the cursor to the beginning of the in-memory file
-        speech_response = audio_bytes
+        audio_base64 = base64.b64encode(audio_data).decode('utf-8')
 
-      return render_template('create_speech.html', create_speech_prompt=form.prompt.data,create_speech_response=speech_response,success=True)
+      return render_template('create_speech.html', create_speech_prompt=form.prompt.data, create_speech_response=audio_base64, success=True)
       
   elif request.method == 'GET':
       return render_template('create_speech.html', form=form)
